fdb6dc4dd067ca61295e5f7400f8473b41c91246
Sarah Maston <maston@pantomath.com>
2015-11-24 10:47:48 -0800

    fixing user seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,19 +11,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = \App\User::firstOrCreate(['email' => 'maston@pantomath.com']);
+        $user = \LMG\User::firstOrCreate(['email' => 'maston@pantomath.com']);
         $user->name = 'maston';
         $user->email = 'maston@pantomath.com';
         $user->password = \Hash::make('abc123');
         $user->save();
 
-        $user = \App\User::firstOrCreate(['email' => 'jill@harvard.edu']);
+        $user = \LMG\User::firstOrCreate(['email' => 'jill@harvard.edu']);
         $user->name = 'Jill';
         $user->email = 'jill@harvard.edu';
         $user->password = \Hash::make('helloworld');
         $user->save();
 
-        $user = \App\User::firstOrCreate(['email' => 'jamal@harvard.edu']);
+        $user = \LMG\User::firstOrCreate(['email' => 'jamal@harvard.edu']);
         $user->name = 'Jamal';
         $user->email = 'jamal@harvard.edu';
         $user->password = \Hash::make('helloworld');
